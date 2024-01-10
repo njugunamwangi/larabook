519f882666bb1f0e59372d70249a30fd4e4c6771
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 18:40:17 +0300

    Profile fields

---
+++ database/migrations/2024_01_10_145659_add_profile_fields_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('display_name')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('photo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('display_name');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('phone_verified_at');
+            $table->dropColumn('photo');
+        });
+    }
+};
