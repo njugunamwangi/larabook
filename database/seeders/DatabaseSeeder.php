df739316cbe72676c1cea0f199c6d7b431829b52
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 14:56:38 +0300

    Register seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,5 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
+        $this->call(OwnerSeeder::class);
+        $this->call(BasicUserSeeder::class);
     }
 }
