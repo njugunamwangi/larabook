ebd5f247899b2c04f36b43f37c7e2d748a8da510
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 18:41:39 +0300

    User and profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,4 +46,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function profile(): HasOne {
+        return $this->hasOne(UserProfile::class);
+    }
 }
---
+++ database/migrations/2024_01_10_145940_create_user_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('invoice_address')->nullable();
+            $table->string('invoice_postcode')->nullable();
+            $table->string('invoice_city')->nullable();
+            $table->foreignId('invoice_country_id')->nullable()->constrained('countries');
+            $table->string('gender')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->foreignId('nationality_country_id')->nullable()->constrained('countries');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
