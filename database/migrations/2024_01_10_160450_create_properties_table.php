016a87cb0446903b2f76638e6b6d10e819216187
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 19:15:06 +0300

    Property model

---
+++ database/migrations/2024_01_10_160450_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained('users');
+            $table->string('name');
+            $table->foreignIdFor(City::class)->constrained();
+            $table->string('address_street');
+            $table->string('address_postcode')->nullable();
+            $table->decimal('lat', 10, 7)->nullable();
+            $table->decimal('long', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
