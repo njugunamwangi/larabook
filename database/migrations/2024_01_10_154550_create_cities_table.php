cda283c322906bbcab85605dda5ed8cea6fb74d0
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 18:52:23 +0300

    City model

---
+++ database/migrations/2024_01_10_154550_create_cities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Country;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Country::class)->constrained();
+            $table->string('name');
+            $table->decimal('lat', 10, 7)->nullable();
+            $table->decimal('long', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
