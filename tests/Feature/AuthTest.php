3ac19b4b2cb1dba04f6f83fb2287ae1b517d960c
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 13:51:01 +0300

    Auth test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_registration_fails_with_admin_role()
+    {
+        $this->artisan('migrate:fresh --seed');
+
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_ADMINISTRATOR
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_registration_succeeds_with_owner_role()
+    {
+        $this->artisan('migrate:fresh --seed');
+
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_OWNER
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+
+    public function test_registration_succeeds_with_user_role()
+    {
+        $this->artisan('migrate:fresh --seed');
+
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_USER
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+}
