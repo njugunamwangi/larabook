edf73907e531954edb7a9153d6146803ff2af795
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 16:12:14 +0300

    Update role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
