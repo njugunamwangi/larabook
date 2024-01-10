4ee5f27e97287ef8e93ef7834afb1ffe4372c0af
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 14:57:18 +0300

    login logic

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('auth/register', RegisterController::class);
+Route::post('auth/login', LoginController::class);
