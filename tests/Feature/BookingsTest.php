43b9e27fb0dc1d1bf954a668620459420d3b698f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 14:58:09 +0300

    Test: booking & property

---
+++ tests/Feature/BookingsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookingsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_has_access_to_bookings_feature()
+    {
+        $this->artisan('seed --PermissionSeeder');
+
+        $owner = User::factory()->create(['role_id' => Role::ROLE_USER]);
+        $response = $this->actingAs($owner)->getJson('/api/user/bookings');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_property_owner_does_not_have_access_to_bookings_feature()
+    {
+        $this->artisan('migrate:fresh --seed');
+
+        $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+        $response = $this->actingAs($owner)->getJson('/api/user/bookings');
+
+        $response->assertStatus(403);
+    }
+}
---
+++ tests/Feature/PropertiesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PropertiesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_property_owner_has_access_to_properties_feature()
+    {
+        $this->artisan('migrate:seed --fresh');
+
+        $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+        $response = $this->actingAs($owner)->getJson('/api/owner/properties');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_does_not_have_access_to_properties_feature()
+    {
+        $this->artisan('migrate:seed --fresh');
+
+        $owner = User::factory()->create(['role_id' => Role::ROLE_USER]);
+        $response = $this->actingAs($owner)->getJson('/api/owner/properties');
+
+        $response->assertStatus(403);
+    }
+}
