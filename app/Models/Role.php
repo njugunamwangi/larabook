c4859e05e68b3187d85dfd72b199d56799cc89ef
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 13:14:19 +0300

    Register api

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,6 +10,10 @@ class Role extends Model
 {
     use HasFactory;
 
+    const ROLE_ADMINISTRATOR = 1;
+    const ROLE_OWNER = 2;
+    const ROLE_USER = 3;
+
     protected $guarded = [];
 
     public function permissions(): BelongsToMany {
