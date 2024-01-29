57137d8aa2fdd870214c0bb479331c0bd428f989
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-29 16:07:40 +0300

    Property search controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Owner\PropertyController;
+use App\Http\Controllers\Public\PropertySearchController;
 use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,10 +20,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::get('owner/properties', [PropertyController::class, 'index']);
-    Route::post('owner/properties',[PropertyController::class, 'store']);
-    Route::get('user/bookings', [BookingController::class, 'index']);
+    Route::prefix('owner')->group(function () {
+        Route::get('properties', [PropertyController::class, 'index']);
+        Route::post('properties',[PropertyController::class, 'store']);
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::get('user/bookings', [BookingController::class, 'index']);
+    });
 });
+
+Route::get('search', PropertySearchController::class);
 
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
