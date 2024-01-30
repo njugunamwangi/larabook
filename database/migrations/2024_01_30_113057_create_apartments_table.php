6b3d05583c817e82a16be1306aa5eb6efb1c5ab9
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-30 15:01:29 +0300

    Apartment model

---
+++ database/migrations/2024_01_30_113057_create_apartments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Property;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Property::class)->constrained();
+            $table->string('name');
+            $table->unsignedInteger('capacity_adults');
+            $table->unsignedInteger('capacity_children');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
