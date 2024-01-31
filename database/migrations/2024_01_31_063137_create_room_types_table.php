22d197fa6486b7180e36d70fa21e26e7a338c15a
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 09:33:41 +0300

    Room type model

---
+++ database/migrations/2024_01_31_063137_create_room_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\RoomType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        RoomType::create(['name'=> 'Bedroom']);
+        RoomType::create(['name'=> 'Living Room']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_types');
+    }
+};
