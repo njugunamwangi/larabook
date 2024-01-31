ad1a7526ea45600e3615919d1f3a9a242d12799c
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 09:55:06 +0300

    Room bed relationship

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
@@ -18,5 +19,9 @@ class Room extends Model
 
     public function apartment() : BelongsTo {
         return $this->belongsTo(Apartment::class);
+    }
+
+    public function beds() : HasMany {
+        return $this->hasMany(Bed::class);
     }
 }
