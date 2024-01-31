1759c0873d8708a4967c34aa6a54ba5893e399f1
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 09:53:25 +0300

    Bed model

---
+++ database/migrations/2024_01_31_065136_create_beds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\BedType;
+use App\Models\Room;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('beds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Room::class)->constrained();
+            $table->foreignIdFor(BedType::class)->constrained();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beds');
+    }
+};
