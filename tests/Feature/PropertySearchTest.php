f0897ad3d588ccad45d893b33da0d8a2e3add3ca
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 14:49:31 +0300

    Test: Bed searches lists all cases

--- tests/Feature/PropertySearchTest.php
+++ tests/Feature/PropertySearchTest.php
@@ -138,4 +138,112 @@ class PropertySearchTest extends TestCase
         $response->assertJsonCount(1, '0.apartments');
         $response->assertJsonPath('0.apartments.0.name', $largeApartment->name);
     }
+
+    public function test_property_search_beds_list_all_cases(): void
+    {
+        $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
+        $cityId = City::value('id');
+        $roomTypes = RoomType::all();
+        $bedTypes = BedType::all();
+
+        $property = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+        ]);
+        $apartment = Apartment::factory()->create([
+            'name' => 'Small apartment',
+            'property_id' => $property->id,
+            'capacity_adults' => 1,
+            'capacity_children' => 0,
+        ]);
+
+        // ----------------------
+        // FIRST: check that bed list if empty if no beds
+        // ----------------------
+
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonCount(1, '0.apartments');
+        $response->assertJsonPath('0.apartments.0.beds_list', '');
+
+        // ----------------------
+        // SECOND: create 1 room with 1 bed
+        // ----------------------
+
+        $room = Room::create([
+            'apartment_id' => $apartment->id,
+            'room_type_id' => $roomTypes[0]->id,
+            'name' => 'Bedroom',
+        ]);
+        Bed::create([
+            'room_id' => $room->id,
+            'bed_type_id' => $bedTypes[0]->id,
+        ]);
+
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '1 ' . $bedTypes[0]->name);
+
+        // ----------------------
+        // THIRD: add another bed to the same room
+        // ----------------------
+
+        Bed::create([
+            'room_id' => $room->id,
+            'bed_type_id' => $bedTypes[0]->id,
+        ]);
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '2 ' . str($bedTypes[0]->name)->plural());
+
+        // ----------------------
+        // FOURTH: add a second room with no beds
+        // ----------------------
+
+        $secondRoom = Room::create([
+            'apartment_id' => $apartment->id,
+            'room_type_id' => $roomTypes[0]->id,
+            'name' => 'Living room',
+        ]);
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '2 ' . str($bedTypes[0]->name)->plural());
+
+        // ----------------------
+        // FIFTH: add one bed to that second room
+        // ----------------------
+
+        Bed::create([
+            'room_id' => $secondRoom->id,
+            'bed_type_id' => $bedTypes[0]->id,
+        ]);
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '3 ' . str($bedTypes[0]->name)->plural());
+
+        // ----------------------
+        // SIXTH: add another bed with a different type to that second room
+        // ----------------------
+
+        Bed::create([
+            'room_id' => $secondRoom->id,
+            'bed_type_id' => $bedTypes[1]->id,
+        ]);
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '4 beds (3 ' . str($bedTypes[0]->name)->plural() . ', 1 ' . $bedTypes[1]->name . ')');
+
+        // ----------------------
+        // SEVENTH: add a second bed with that new type to that second room
+        // ----------------------
+
+        Bed::create([
+            'room_id' => $secondRoom->id,
+            'bed_type_id' => $bedTypes[1]->id,
+        ]);
+        $response = $this->getJson('/api/search?city=' . $cityId);
+        $response->assertStatus(200);
+        $response->assertJsonPath('0.apartments.0.beds_list', '5 beds (3 ' . str($bedTypes[0]->name)->plural() . ', 2 ' . str($bedTypes[1]->name)->plural() . ')');
+    }
 }
