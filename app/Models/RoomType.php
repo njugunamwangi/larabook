41ff4353f56beff7c65b598ef33aa86ab93fde9f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 09:49:10 +0300

    Room room type relationship

--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RoomType extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function rooms(): HasMany {
+        return $this->hasMany(Room::class);
+    }
 }
