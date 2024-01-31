b8cca00c114d83ebf252cf09e35d9e4ab27ff7ee
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-31 14:51:16 +0300

    Disable seeders for tests to run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,9 +31,9 @@ class DatabaseSeeder extends Seeder
         $this->call(CitySeeder::class);
         $this->call(GeoObjectSeeder::class);
 
-        $this->call(PropertySeeder::class);
-        $this->call(ApartmentSeeder::class);
-        $this->call(RoomSeeder::class);
-        $this->call(BedSeeder::class);
+        // $this->call(PropertySeeder::class);
+        // $this->call(ApartmentSeeder::class);
+        // $this->call(RoomSeeder::class);
+        // $this->call(BedSeeder::class);
     }
 }
