c4108d8fcd9ee4e03fee3757f01ab57c8ccc8212
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-06 21:20:36 +0300

    Implement property model has media

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -9,12 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Staudenmeir\EloquentEagerLimit\HasEagerLimit;
 
-class Property extends Model
+class Property extends Model implements HasMedia
 {
     use HasFactory;
     use HasEagerLimit;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
