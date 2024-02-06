66d7fa33b27b774f6629d2ac0275504a87da7a76
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-06 20:54:19 +0300

    Bug fix

--- tests/Feature/PropertySearchTest.php
+++ tests/Feature/PropertySearchTest.php
@@ -279,10 +279,35 @@ class PropertySearchTest extends TestCase
             'capacity_children' => 0,
         ]);
 
+        $property2 = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+        ]);
+        Apartment::factory()->create([
+            'name' => 'Large apartment 2',
+            'property_id' => $property2->id,
+            'capacity_adults' => 3,
+            'capacity_children' => 2,
+        ]);
+        Apartment::factory()->create([
+            'name' => 'Mid size apartment 2',
+            'property_id' => $property2->id,
+            'capacity_adults' => 2,
+            'capacity_children' => 1,
+        ]);
+        Apartment::factory()->create([
+            'name' => 'Small apartment 2',
+            'property_id' => $property2->id,
+            'capacity_adults' => 1,
+            'capacity_children' => 0,
+        ]);
+
         $response = $this->getJson('/api/search?city=' . $cityId . '&adults=2&children=1');
 
         $response->assertStatus(200);
+        $response->assertJsonCount(2,'properties');
         $response->assertJsonCount(1, 'properties.0.apartments');
+        $response->assertJsonCount(1, 'properties.1.apartments');
         $response->assertJsonPath('properties.0.apartments.0.name', $midSizeApartment->name);
     }
 
