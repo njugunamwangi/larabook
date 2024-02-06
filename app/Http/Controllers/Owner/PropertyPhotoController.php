136d4eb6050bc30500054f1299fa55a11b10bd8e
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-06 21:20:49 +0300

    Store photo

---
+++ app/Http/Controllers/Owner/PropertyPhotoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use App\Models\Property;
+use Illuminate\Http\Request;
+
+class PropertyPhotoController extends Controller
+{
+    public function  store(Property $property, Request $request) {
+        $request->validate([
+            'photo' => ['image', 'max:5000']
+        ]);
+
+        if ($property->owner_id != auth()->id()) {
+            abort(403);
+        }
+
+        $photo = $property->addMediaFromRequest('photo')->toMediaCollection('photos');
+
+        return [
+            'filename' => $photo->getUrl(),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Public\PropertyController as PublicPropertyController;
 use App\Http\Controllers\Owner\PropertyController as OwnerPropertyController;
+use App\Http\Controllers\Owner\PropertyPhotoController;
 use App\Http\Controllers\Public\ApartmentController;
 use App\Http\Controllers\Public\PropertySearchController;
 use App\Http\Controllers\User\BookingController;
@@ -25,6 +26,7 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::prefix('owner')->group(function () {
         Route::get('properties', [OwnerPropertyController::class, 'index']);
         Route::post('properties',[OwnerPropertyController::class, 'store']);
+        Route::post('properties/{property}/photos', [PropertyPhotoController::class, 'store']);
     });
 
     Route::prefix('user')->group(function () {
