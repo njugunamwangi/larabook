17973c51a1dcede8c5eeb25c0058f96fc0b52346
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-15 11:24:11 +0300

    Test: Owner can add property photo

--- tests/Feature/PropertiesTest.php
+++ tests/Feature/PropertiesTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\City;
+use App\Models\Property;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class PropertiesTest extends TestCase
@@ -44,5 +47,27 @@ class PropertiesTest extends TestCase
 
         $response->assertSuccessful();
         $response->assertJsonFragment(['name' => 'My property']);
+    }
+
+    public function test_property_owner_can_add_photo_to_property()
+    {
+        Storage::fake();
+
+        $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
+        $cityId = City::value('id');
+        $property = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+        ]);
+
+        $response = $this->actingAs($owner)->postJson('/api/owner/properties/' . $property->id . '/photos', [
+            'photo' => UploadedFile::fake()->image('photo.png')
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'filename' => config('app.url') . '/storage/1/photo.png',
+            'thumbnail' => config('app.url') . '/storage/1/conversions/photo-thumbnail.jpg',
+        ]);
     }
 }
