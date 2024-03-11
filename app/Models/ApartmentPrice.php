8ba9ceff5dbcbacc33fc91efc5563d70961be8db
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-11 17:51:03 +0300

    Refactor valid for range scope

--- app/Models/ApartmentPrice.php
+++ app/Models/ApartmentPrice.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\ValidForRange;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ApartmentPrice extends Model
 {
     use HasFactory;
+    use ValidForRange;
 
     protected $guarded = [];
 
@@ -20,25 +22,5 @@ class ApartmentPrice extends Model
 
     public function apartment(): BelongsTo {
         return $this->belongsTo(Apartment::class);
-    }
-
-    public function scopeValidForRange($query, array $range = [])
-    {
-        return $query->where(function ($query) use ($range) {
-            return $query
-                // Covers outer bounds
-                ->where(function ($query) use ($range) {
-                    $query->where('start_date', '>=', reset($range))->where('end_date', '<=', end($range));
-                })
-                // Covers left and right bound
-                ->orWhere(function ($query) use ($range) {
-                    $query->whereBetween('start_date', $range)->orWhereBetween('end_date', $range);
-                })
-                // Covers inner bounds
-                ->orWhere(function ($query) use ($range) {
-                    $query->where('start_date', '<=', reset($range))
-                        ->where('end_date', '>=', end($range));
-                });
-        });
     }
 }
