95d64b9ce12d71f1c1d0b9904b4110c4b1ca229d
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-11 17:59:12 +0300

    Booking controller resource

--- app/Http/Controllers/User/BookingController.php
+++ app/Http/Controllers/User/BookingController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingRequest;
+use App\Http\Resources\BookingResource;
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -11,8 +14,44 @@ class BookingController extends Controller
     {
         $this->authorize('bookings-manage');
 
-        // Will implement booking management later
-        return response()->json(['success' => true]);
+        $bookings = auth()->user()->bookings()
+            ->with('apartment.property')
+            ->withTrashed()
+            ->orderBy('start_date')
+            ->get();
 
+        return BookingResource::collection($bookings);
+
+    }
+
+    public function store(StoreBookingRequest $request)
+    {
+        $booking = auth()->user()->bookings()->create($request->validated());
+
+        return new BookingResource($booking);
+    }
+
+    public function show(Booking $booking)
+    {
+        $this->authorize('bookings-manage');
+
+        if ($booking->user_id != auth()->id()) {
+            abort(403);
+        }
+
+        return new BookingResource($booking);
+    }
+
+    public function destroy(Booking $booking)
+    {
+        $this->authorize('bookings-manage');
+
+        if ($booking->user_id != auth()->id()) {
+            abort(403);
+        }
+
+        $booking->delete();
+
+        return response()->noContent();
     }
 }
